cf16e32349b317e05eab2e8406dd4102c4baceac
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2022-02-24 19:32:39 +0100

    Nivell 1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+interface JokeResponse {
+  id: string;
+  joke: string;
+  status: number
+}
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+interface jokeQuality {
+  joke: string;
+  score: 1 | 2 | 3;
+  date: string;
+}
+
+let reportJokes: jokeQuality[] = [];
+
+const JOKEAPI = `https://icanhazdadjoke.com/`;
+const jokeElement = <HTMLParagraphElement>document.querySelector<HTMLParagraphElement>('.joke')
+
+const getJoke = async (): Promise<JokeResponse> => {
+  const response = await fetch(JOKEAPI, {
+    headers: {
+      accept: 'application/json'
+    }
+  });
+
+  const jokeResponse: JokeResponse = await response.json();
+
+  console.log(jokeResponse.joke);
+  jokeElement.innerText = jokeResponse.joke;
+  return jokeResponse;
+}
+
+const addScore = async (score: 1 | 2 | 3): Promise<JokeResponse> => {
+  const joke: string = <string>jokeElement.innerText;
+  const timeOfVoting = new Date();
+  const date = timeOfVoting.toISOString();
+
+  const reportJoke: jokeQuality = {
+    joke,
+    score,
+    date
+  }
+
+  reportJokes.push(reportJoke);
+
+  console.log(`In this sesion the user has seen a total of ${reportJokes.length} and this are the scores:`, reportJokes);
+
+  return await getJoke();
+}
+
+getJoke();
+
+(<any>window).getJoke = getJoke;
+(<any>window).addScore = addScore;
